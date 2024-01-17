a5569f825c51fa6674d4203f3a6df30cd783d20c
Yaya SOW <yaya.sowzone@gmail.com.com>
2024-01-17 18:04:46 +0000

    Ajout_RP

---
+++ Views/demandes/liste_class.rp.html.php
@@ -0,0 +1,54 @@
+<div class="bod">
+        <fieldset class="container">
+            <legend align="center">Liste Des Classes</legend>
+            <div class="click0">
+                <div class="filtre">
+                    <span class="etat">Nom & Prenom Prof</span>
+                    <input type="text">
+                    <button type="submit">Ok</button>
+                </div>
+            </div>
+            <div class="tab">
+                <div class="table">
+                    <table>
+                        <tr>
+                            <th>Libelle</th>
+                            <th>Filiere</th>
+                            <th>Niveau</th>
+                        </tr>
+                        <tr>
+                            <td>L2-GLRS</td>
+                            <td>GLRS</td>
+                            <td>L2</td>
+                        </tr>
+                        <tr>
+                            <td>L2-CDSD</td>
+                            <td>CDSD</td>
+                            <td>L2</td>
+                        </tr>
+                        <tr>
+                            <td>L2-ETSE</td>
+                            <td>ETSE</td>
+                            <td>L2</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="Creer">
+                    <a href="wire 2 RP.html"><button type="submit">Creer Une Classe</button></a>
+                </div>
+            </div>
+            <div class="page">
+                <a href="wire 1 Etudiant.html" class="li">
+                    <span>&laquo; Precedent &raquo;</span>
+                </a>
+                <div class="link">
+                    <a href="" class="active">1</a>
+                    <a href="" class="">2</a>
+                    <a href="" class="">3</a>
+                </div>
+                <a href="wire 1,1 Etudiant.html" class="li">
+                    <span>&laquo; Suivant &raquo;</span>
+                </a>
+            </div>
+        </fieldset>
+</div>
